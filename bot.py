489df380e0270e07689fd5457ff6d8c52c5b4113
AshankKumar <ashankkumar01@gmail.com>
2022-03-03 17:29:53 -0600

    Added back on_join functionality

--- bot.py
+++ bot.py
@@ -2,13 +2,30 @@
 import os
 import random
 
+from discord import Intents
 from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-bot = commands.Bot(command_prefix='!')
+intents = Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+
+@bot.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
 
 
 @bot.command(name='99')
