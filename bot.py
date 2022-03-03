a7324db18e36069d277b0405a942a75eae17c340
AshankKumar <ashankkumar01@gmail.com>
2022-03-03 17:09:48 -0600

    Added basic func from tutorial

--- bot.py
+++ bot.py
@@ -1,26 +1,47 @@
 # bot.py
 import os
-
+import random
 import discord
 from dotenv import load_dotenv
 
+intents = discord.Intents.default()
+intents.members = True
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+client = discord.Client(intents=intents)
 
 
 @client.event
 async def on_ready():
-    # print(f'{client.user} has connected to Discord!')
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+    print(f'{client.user.name} has connected to Discord!')
 
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
     )
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
 
 client.run(TOKEN)
